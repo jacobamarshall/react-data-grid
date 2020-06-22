9534a1d680d0cd77f84851a7afd41a43881f1960
Jacob <jacobm43@uw.edu>
2020-06-22 14:10:36 -0700

    Added pointer event capabilities for mobile in CheckboxEditor

--- packages/common/editors/CheckboxEditor.js
+++ packages/common/editors/CheckboxEditor.js
@@ -1,5 +1,6 @@
 const React = require('react');
 import PropTypes from 'prop-types';
+import { isMobile } from "react-device-detect";
 require('../../../themes/react-data-grid-checkbox.css');
 
 class CheckboxEditor extends React.Component {
@@ -13,18 +14,40 @@ class CheckboxEditor extends React.Component {
     dependentValues: PropTypes.object
   };
 
+  constructor(props) {
+    super(props);
+  };
+
   handleChange = (e) => {
+    this.props.column.onCellChange(this.props.rowIdx, this.props.column.key, this.props.dependentValues, e);
+  };
+
+  handlePointerEnter = (e) => {
     this.props.column.onCellChange(this.props.rowIdx, this.props.column.key, this.props.dependentValues, e);
   };
 
   render() {
     const checked = this.props.value != null ? this.props.value : false;
     const checkboxName = 'checkbox' + this.props.rowIdx;
-    return (
-      <div className="react-grid-checkbox-container checkbox-align" onClick={this.handleChange}>
+    //use onTouch if mobile, onclick if not mobile
+    if (isMobile) {
+      return (
+        <div className="react-grid-checkbox-container checkbox-align"
+             onPointerEnter={this.handlePointerEnter}
+        >
           <input className="react-grid-checkbox" type="checkbox" name={checkboxName} checked={checked} readOnly />
           <label htmlFor={checkboxName} className="react-grid-checkbox-label"></label>
-      </div>);
+        </div>);
+    }
+    else {
+      return (
+        <div className="react-grid-checkbox-container checkbox-align"
+          onClick={this.handleChange}
+        >
+          <input className="react-grid-checkbox" type="checkbox" name={checkboxName} checked={checked} readOnly />
+          <label htmlFor={checkboxName} className="react-grid-checkbox-label"></label>
+        </div>);
+    }
   }
 }
 
